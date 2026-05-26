377747326ed5731f23f5a51ebf25fa4c727ce637
Мирон <mera@Macintosh.local>
2026-05-26 16:43:31 +0400

    внёс правки

--- lab3-var3/file_utils.py
+++ lab3-var3/file_utils.py
@@ -1,10 +1,32 @@
 def write_binary_file(path, data):
+    """
+    Сохраняет бинарные данные в файл.
 
+    Файл открывается в режиме бинарной записи.
+    Если файл не существует, он будет создан.
+
+    Args:
+        path (str):
+            Путь к сохраняемому файлу.
+
+        data (bytes):
+            Данные для записи.
+    """
     with open(path, 'wb') as file:
         file.write(data)
 
 
 def read_binary_file(path):
+    """
+    Считывает бинарные данные из файла.
 
+    Args:
+        path (str):
+            Путь к считываемому файлу.
+
+    Returns:
+        bytes:
+            Содержимое файла в виде массива байтов.
+    """
     with open(path, 'rb') as file:
         return file.read()
